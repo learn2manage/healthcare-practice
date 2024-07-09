23e4e9e2d5d80ebc74a8e8c825b7da0c1b2b6a06
Learn2manage <learn2manage@gmail.com>
2024-07-09 21:41:15 +0800

    Prepare the 3 input fields in PatientForm.

--- components/ui/CustomerFormField.tsx
+++ components/ui/CustomerFormField.tsx
@@ -1,92 +1,109 @@
-"use client";
+'use client';
 
 import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Control } from "react-hook-form";
-import { FormFieldType } from "../forms/PatientForm";
-import Image from "next/image";
-
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Control } from 'react-hook-form';
+import { FormFieldType } from '../forms/PatientForm';
+import Image from 'next/image';
+import PhoneInput from 'react-phone-number-input/input';
+import { E164Number } from 'libphonenumber-js/core';
+import PhoneInputWithCountrySelect from 'react-phone-number-input';
 interface HomeFormValues {
-  username: string;
+    username: string;
 }
 
 interface CustomerProps {
-  control: Control<any>;
-  fieldType: FormFieldType;
-  name: string;
-  label?: string;
-  placeholder?: string;
-  iconSrc?: string;
-  iconAlt?: string;
-  disabled?: boolean;
-  dateFormat?: string;
-  showTimesSelect?: boolean;
-  childer?: React.ReactNode;
-  renderSkeleton?: (field: any) => React.ReactNode;
+    control: Control<any>;
+    fieldType: FormFieldType;
+    name: string;
+    label?: string;
+    placeholder?: string;
+    iconSrc?: string;
+    iconAlt?: string;
+    disabled?: boolean;
+    dateFormat?: string;
+    showTimesSelect?: boolean;
+    childer?: React.ReactNode;
+    renderSkeleton?: (field: any) => React.ReactNode;
 }
 
 const RenderField = ({
-  field,
-  props,
+    field,
+    props,
 }: {
-  field: any;
-  props: CustomerProps;
+    field: any;
+    props: CustomerProps;
 }) => {
-  const { fieldType, iconSrc, iconAlt, placeholder } = props;
+    const { fieldType, iconSrc, iconAlt, placeholder } = props;
 
-  switch (fieldType) {
-    case FormFieldType.INPUT:
-      return (
-        <div className="flex rounded-md border border-dark-500 bg-dark-400">
-          {iconSrc && (
-            <Image
-              src={iconSrc}
-              height={24}
-              width={24}
-              alt={iconAlt || "icon"}
-              className="ml-2"
-            />
-          )}
+    switch (fieldType) {
+        case FormFieldType.INPUT:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    {iconSrc && (
+                        <Image
+                            src={iconSrc}
+                            height={24}
+                            width={24}
+                            alt={iconAlt || 'icon'}
+                            className="ml-2"
+                        />
+                    )}
 
-          <FormControl>
-            <Input
-              placeholder={placeholder}
-              {...field}
-              className="shad-input border-0"
-            />
-          </FormControl>
-        </div>
-      );
-  }
+                    <FormControl>
+                        <Input
+                            placeholder={placeholder}
+                            {...field}
+                            className="shad-input border-0"
+                        />
+                    </FormControl>
+                </div>
+            );
+        case FormFieldType.PHONE_INPUT:
+            return (
+                <FormControl>
+                    <PhoneInputWithCountrySelect
+                        defaultCountry="US"
+                        placeholder={placeholder}
+                        international
+                        withCountryCallingCode
+                        value={field.value as E164Number | undefined}
+                        onChange={field.onChange}
+                        className="input-phone"
+                    />
+                </FormControl>
+            );
+        default:
+            break;
+    }
 };
 
 const CustomerFormField = (props: CustomerProps) => {
-  const { control, fieldType, name, label } = props;
-  return (
-    <FormField
-      control={control}
-      name={name}
-      render={({ field }) => (
-        <FormItem className="flex-1">
-          {fieldType !== FormFieldType.CHECKBOX && label && (
-            <FormLabel>{label}</FormLabel>
-          )}
-          <RenderField
-            field={field}
-            props={props}
-          />
-          <FormMessage className="shad-error" />
-        </FormItem>
-      )}
-    />
-  );
+    const { control, fieldType, name, label } = props;
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem className="flex-1">
+                    {fieldType !== FormFieldType.CHECKBOX && label && (
+                        <FormLabel className="shad-input-label">
+                            {label}
+                        </FormLabel>
+                    )}
+                    <RenderField field={field} props={props} />
+                    <FormMessage className="shad-error" />
+                </FormItem>
+            )}
+        />
+    );
 };
 
 export default CustomerFormField;
