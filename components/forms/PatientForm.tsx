52d4635f010a88e9c0ccb9bc30bb05d97bac7d39
Ian Ho <ian@smarts.hk>
2024-07-10 17:30:17 +0800

    Adding appwrite, unfinished.

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,96 +1,98 @@
-'use client';
+"use client";
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { Button } from '@/components/ui/button';
-import { Form } from '@/components/ui/form';
-import 'react-phone-number-input/style.css';
-import CustomFormField from '../CustomFormField';
-import SubmitButton from '../SubmitButton';
-import { useState } from 'react';
-import { UserFormValidation } from '@/lib/validation';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
+import "react-phone-number-input/style.css";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
-    INPUT = 'input',
-    TEXTAREA = 'textarea',
-    PHONE_INPUT = 'phoneInput',
-    CHECKBOX = 'checkbox',
-    DATE_PICKER = 'datepicker',
-    SELECT = 'select',
-    SKELETON = 'skeleton',
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datepicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
 }
 
 const PatientForm = () => {
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
-    const form = useForm<z.infer<typeof UserFormValidation>>({
-        resolver: zodResolver(UserFormValidation),
-        defaultValues: {
-            name: '',
-            email: '',
-            phone: '',
-        },
-    });
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
 
-    // 2. Define a submit handler.
-    async function onSubmit({
-        name,
-        email,
-        phone,
-    }: z.infer<typeof UserFormValidation>) {
-        setIsLoading(true);
-        try {
-            const userData = { name, email, phone };
-            console.log(userData);
+  // 2. Define a submit handler.
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      const userData = { name, email, phone };
+      console.log(userData);
+      //const user = await createUser(userData);
 
-            setIsLoading(false);
-        } catch (error) {
-            console.log(error);
-        }
+      //if (user) router.push(`/patients/${user.$id}/register`);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8 flex-1">
-                <section className="mb-12 space-y-4">
-                    <h1 className="header">Hi there ✋</h1>
-                    <p className="text-dark-700">
-                        Schedule your first appointment.
-                    </p>
-                </section>
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there ✋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
 
-                <CustomFormField
-                    fieldType={FormFieldType.INPUT}
-                    control={form.control}
-                    name="name"
-                    label="Full user name"
-                    placeholder="Elon Musk"
-                    iconSrc="/assets/icons/user.svg"
-                    iconAlt="user"
-                />
-                <CustomFormField
-                    fieldType={FormFieldType.INPUT}
-                    control={form.control}
-                    name="email"
-                    label="Email Address"
-                    placeholder="test@abc.com"
-                    iconSrc="/assets/icons/email.svg"
-                    iconAlt="email"
-                />
-                <CustomFormField
-                    fieldType={FormFieldType.PHONE_INPUT}
-                    control={form.control}
-                    name="phone"
-                    label="Phone Number"
-                    placeholder="(555) 234-1232"
-                />
-                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
-            </form>
-        </Form>
-    );
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full user name"
+          placeholder="Elon Musk"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email Address"
+          placeholder="test@abc.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone Number"
+          placeholder="(555) 234-1232"
+        />
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;
